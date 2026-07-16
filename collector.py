5397cf1e1561722ad47f0f9d8f9f132476127258
Bryan Tabares <tabaresbryan39@gmail.com>
2026-07-17 00:06:32 +0800

    fix: added main funcs

--- collector.py
+++ collector.py
@@ -9,7 +9,8 @@ logging.basicConfig(
 )
 
 BASE_URL = "https://jobs.govt.nz"
-DATA_DIR = "./data"
+SOURCE_NAME = 'jobs_govt_nz'
+DATA_DIR = f"./raw_data/{SOURCE_NAME}"
 
 HEADERS = {
     "Content-Type": "application/x-www-form-urlencoded",
@@ -31,67 +32,68 @@ BODY = {
     "in_searchBut": "",
     "in_pg": "0",
 }
+def main():
+    page = "0"
+    next_page = 1
+    total_jobs = 0
+    while True:
+        logging.info(f"Fetching jobs from page # {next_page}")
+        if total_jobs and int(page) >= int(total_jobs):
+            break
+        BODY['in_pg']=page
+        response = requests.post(
+            url=BASE_URL+"/jobtools/jncustomsearch.searchResults",
+            headers=HEADERS,
+            data=BODY,
+        )
+        page=str(next_page*20)
+        next_page+=1
 
-page = "0"
-next_page = 1
-total_jobs = 0
-while True:
-    logging.info(f"Fetching jobs from page # {next_page}")
-    if total_jobs and int(page) >= int(total_jobs):
-        break
-    BODY['in_pg']=page
-    response = requests.post(
-        url=BASE_URL+"/jobtools/jncustomsearch.searchResults",
-        headers=HEADERS,
-        data=BODY,
-    )
-    page=str(next_page*20)
-    next_page+=1
+        all_jobs=[]
+        parser = BeautifulSoup(response.text, "html.parser")
+        if not total_jobs:
+            total_jobs = parser.find("input",attrs={"name":"in_totalrows"})
+            total_jobs = total_jobs.get("value") if total_jobs else 0
+            logging.info(f"Found {total_jobs} jobs")
 
-    all_jobs=[]
-    parser = BeautifulSoup(response.text, "html.parser")
-    if not total_jobs:
-        total_jobs = parser.find("input",attrs={"name":"in_totalrows"})
-        total_jobs = total_jobs.get("value") if total_jobs else 0
-        logging.info(f"Found {total_jobs} jobs")
+        for tr in parser.find_all('tr'):
+            title_td = tr.find('td', class_='job_title')
+            if not title_td:
+                continue
+            first_link = title_td.find('a')
+            
+            job = { 
+                "job_title_text": first_link.text.strip() if first_link else None,
+                "job_url": first_link['href'] if first_link else None,
+                "company": title_td.find('div', class_=None).text.replace('at ', '').strip() if title_td.find('div', class_=None) else None,
+            }
+            
+            all_jobs.append(job)
+        # FIXME: This is redundant
+        job_links= []
+        for job in all_jobs:
+            job_url: str = job.get('job_url')
+            details_url_base = BASE_URL
+            if not job_url.startswith("/jobs"):
+                details_url_base += "/jobtools/"
+            job_links.append(details_url_base + job_url)
 
-    for tr in parser.find_all('tr'):
-        title_td = tr.find('td', class_='job_title')
-        if not title_td:
-            continue
-        first_link = title_td.find('a')
-        
-        job = { 
-            "job_title_text": first_link.text.strip() if first_link else None,
-            "job_url": first_link['href'] if first_link else None,
-            "company": title_td.find('div', class_=None).text.replace('at ', '').strip() if title_td.find('div', class_=None) else None,
-        }
-        
-        all_jobs.append(job)
-    # FIXME: This is redundant
-    job_links= []
-    for job in all_jobs:
-        job_url: str = job.get('job_url')
-        details_url_base = BASE_URL
-        if not job_url.startswith("/jobs"):
-            details_url_base += "/jobtools/"
-        job_links.append(details_url_base + job_url)
-
-    job_details = []
-    for idx, job in enumerate(job_links):
-        job_deets = requests.get(job)
-        details_parser= BeautifulSoup(job_deets.text, 'html.parser')
-        details_table = details_parser.select_one("div[class^='job-details']")
-        job_reference = None
-        if details_table:
-            for row in details_table.find_all('tr'):
-                if "Reference" in row.find_all('td')[0].get_text(strip=True):
-                    job_reference = row.find_all('td')[1].get_text(strip=True)
-                    break
-        filename = job_reference or all_jobs[idx]['job_title_text']
-        job_filename = f'{DATA_DIR}/{filename.replace('/','_')}.html'
-        logging.info(f"Downloading ref# {job_filename} - {all_jobs[idx]['job_title_text']}")
-        #job_details.append(details_table)
-        with open(job_filename,'w+') as html_file:
-            html_file.write(details_parser.prettify())
-        
+        job_details = []
+        for idx, job in enumerate(job_links):
+            job_deets = requests.get(job)
+            details_parser= BeautifulSoup(job_deets.text, 'html.parser')
+            details_table = details_parser.select_one("div[class^='job-details']")
+            job_reference = None
+            if details_table:
+                for row in details_table.find_all('tr'):
+                    if "Reference" in row.find_all('td')[0].get_text(strip=True):
+                        job_reference = row.find_all('td')[1].get_text(strip=True)
+                        break
+            filename = job_reference or all_jobs[idx]['job_title_text']
+            job_filename = f'{DATA_DIR}/{filename.replace('/','_')}.html'
+            logging.info(f"Downloading ref# {job_filename} - {all_jobs[idx]['job_title_text']}")
+            #job_details.append(details_table)
+            with open(job_filename,'w+') as html_file:
+                html_file.write(details_parser.prettify())
+if __name__ == "__main__":
+    main()
--- parser.py
+++ parser.py
@@ -7,32 +7,35 @@ source_name = 'jobs_govt_nz'
 base_dir = f'./raw_data/{source_name}/'
 files_list = os.listdir(base_dir)
 
-jobs =[]
-for file in files_list:
-    with open(os.path.join(base_dir,file), 'r') as f:
-        data = f.read()
+def main():
+    jobs =[]
+    for file in files_list:
+        with open(os.path.join(base_dir,file), 'r') as f:
+            data = f.read()
 
 
-    soup = BeautifulSoup(data, "html.parser")
-    details_table = soup.select_one("div[class^='job-details']")
-    raw_title = soup.find('title')
-    job_title = ""
-    if raw_title:
-        job_title = raw_title.text.split('|')[0].strip()
+        soup = BeautifulSoup(data, "html.parser")
+        details_table = soup.select_one("div[class^='job-details']")
+        raw_title = soup.find('title')
+        job_title = ""
+        if raw_title:
+            job_title = raw_title.text.split('|')[0].strip()
 
-    job_desc_raw = soup.find("div", class_="jobDesc")
-    job_desc = ""
-    if job_desc_raw:
-        job_desc = re.sub(r'\n+', r'\n',(job_desc_raw.text).strip()) # re.sub removes extra \n (s)
+        job_desc_raw = soup.find("div", class_="jobDesc")
+        job_desc = ""
+        if job_desc_raw:
+            job_desc = re.sub(r'\n+', r'\n',(job_desc_raw.text).strip()) # re.sub removes extra \n (s)
 
-    job = { "job_title": job_title }
-    if details_table:
-        for row in details_table.find_all('tr'):
-            first_td = row.find_all('td')[0].get_text(strip=True)
-            second_td = row.find_all('td')[1].get_text(strip=True)
-            job[first_td.lower().replace(":","").replace(" ","_")] = second_td
-        jobs.append(job)
+        job = { "job_title": job_title }
+        if details_table:
+            for row in details_table.find_all('tr'):
+                first_td = row.find_all('td')[0].get_text(strip=True)
+                second_td = row.find_all('td')[1].get_text(strip=True)
+                job[first_td.lower().replace(":","").replace(" ","_")] = second_td
+            jobs.append(job)
 
-with open('parsed.json', 'w+') as file:
-    for job in jobs:
-        file.write(json.dumps(job) + "\n")
+    with open('parsed.json', 'w+') as file:
+        for job in jobs:
+            file.write(json.dumps(job) + "\n")
+if __name__ == "__main__":
+    main()
